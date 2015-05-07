2fabeb61c7a8320a60ad9f79c06aec8dd876502c
Matt Casey <matt@digital-ice.co.uk>
2015-05-07 19:28:47 +0100

    Initial Commit

---
+++ SwiftStub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftStub
+//
+//  Created by Matt Casey on 07/05/2015.
+//  Copyright (c) 2015 Digital Ice Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
